ce95cc836c1a247e1d31b4d8e90a4cc9c5a1690a
Lukas Novak <novakl39@fel.cvut.cz>
2022-05-15 00:24:52 +0200

    Add Invoice model, services and rest endpoints

---
+++ reservation-system/invoice-service/src/main/java/cz/cvut/fel/invoice/service/InvoiceService.java
@@ -0,0 +1,20 @@
+package cz.cvut.fel.invoice.service;
+
+import cz.cvut.fel.invoice.model.Invoice;
+
+import java.util.List;
+
+public interface InvoiceService {
+
+    Invoice findById(Integer id) throws Exception;
+
+    List<Invoice> getMyInvoices();
+
+    void save(Invoice invoice) throws Exception;
+
+    Invoice findByIdAdmin(Integer id);
+
+    List<Invoice> getUserInvoicesAdmin(Integer id);
+
+    void deleteById(Integer id);
+}
